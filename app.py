f988ef2f7230b8b685b28d6b3304d1da0d399027
MuhammadArsalan1996 <52852763+MuhammadArsalan1996@users.noreply.github.com>
2020-10-04 03:51:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,61 +23,7 @@ IMS_model = pickle.load(open('ims.pkl', 'rb'))
 @app.route('/')
 def welcome():
     return "Welcome All"
-@app.route('/predict',methods=["Get"])
-def predict_note_authentication(Diabetes_Type1,Diabetes_Type2,liverDisease
-,heartDisease, kidneyDisease,Flu,Fever,LowBP,HighBP):
 
-   
-    prediction=IMS_model.predict([[Diabetes_Type1,Diabetes_Type2,liverDisease
-,heartDisease, kidneyDisease,Flu,Fever,LowBP,HighBP]])
-    print(prediction)
-    return prediction
-
-
-
-def main():
-    st.title("Select your Disease if necessary")
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-   
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    Diabetes_Type1 = st.checkbox("Diabetes_Type1")
-    if Diabetes_Type1:
-     st.checkbox("yes", value = True,key=0);
-    Diabetes_Type2 = st.checkbox("Diabetes_Type2")
-    if DiabetesTypeTwo:
-     st.checkbox("yes", value = True,key=1)
-    liverDisease = st.checkbox("liverDisease")
-    if liverDisease:
-     st.checkbox("yes", value = True,key=2)
-    heartDisease = st.checkbox("heartDisease")
-    if heartDisease:
-     st.checkbox("yes", value = True,key=3)
-    kidneyDisease = st.checkbox("kidneyDisease")
-    if kidneyDisease:
-     st.checkbox("yes", value = True,key=4)
-    Flu = st.checkbox("Flu")
-    if Flu:
-     st.checkbox("yes", value = True,key=5)
-    Fever = st.checkbox("Fever" )
-    if Fever:
-     st.checkbox("yes", value = True,key=6)
-    LowBP = st.checkbox("LowBP" )
-    if LowBP:
-     st.checkbox("yes", value = True,key=7)
-    HighBP= st.checkbox("HighBP")
-    if HighBP:
-     st.checkbox("yes", value = True,key=8)
-    result=""
-    if st.button("Predict"):
-      result=predict_note_authentication(Diabetes_Type1,Diabetes_Type2,liverDisease,heartDisease, kidneyDisease,Flu,Fever,LowBP,HighBP)
-    st.success('Recommended {}'.format(result)) 
-   
-   
-       
-        
         #Mobile App Api
 @app.route('/predict_api',methods=['GET', 'POST'])
 def predict_api():
