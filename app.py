56c533624a262020f896f2b56b118c915701134e
MuhammadArsalan1996 <52852763+MuhammadArsalan1996@users.noreply.github.com>
2020-10-04 04:16:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,25 +23,59 @@ IMS_model = pickle.load(open('ims.pkl', 'rb'))
 @app.route('/')
 def welcome():
     return "Welcome All"
+@app.route('/predict',methods=["Get"])
+def predict_note_authentication(Diabetes_Type1,Diabetes_Type2,liverDisease
+,heartDisease, kidneyDisease,Flu,Fever,LowBP,HighBP):
 
-        #Mobile App Api
-@app.route('/predict_api',methods=['GET', 'POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''   
-    data = request.get_json(force=True)
-    final_features = [np.array(data)]
-    recommended  =IMS_model.predict(final_features)
-    recommended2 =IMS_model.predict(final_features)
-    recommended3 =IMS_model.predict(final_features)
-    recommended4 =IMS_model.predict(final_features)
-    recommended5 =IMS_model.predict(final_features)
    
+    prediction=IMS_model.predict([[Diabetes_Type1,Diabetes_Type2,liverDisease
+,heartDisease, kidneyDisease,Flu,Fever,LowBP,HighBP]])
+    print(prediction)
+    return prediction
 
-    
-    return jsonify(recommended=recommended[0][0],recommended2=recommended2[0][1],recommended3=recommended3[0][2],recommended4=recommended4[0][3],recommended5=recommended5[0][4])
-     
+
+
+def main():
+    st.title("Select your Disease if necessary")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+   
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    Diabetes_Type1 = st.checkbox("Diabetes_Type1")
+    if Diabetes_Type1:
+     st.checkbox("yes", value = True,key=0);
+    Diabetes_Type2 = st.checkbox("Diabetes_Type2")
+    if Diabetes_Type2:
+     st.checkbox("yes", value = True,key=1)
+    liverDisease = st.checkbox("liverDisease")
+    if liverDisease:
+     st.checkbox("yes", value = True,key=2)
+    heartDisease = st.checkbox("heartDisease")
+    if heartDisease:
+     st.checkbox("yes", value = True,key=3)
+    kidneyDisease = st.checkbox("kidneyDisease")
+    if kidneyDisease:
+     st.checkbox("yes", value = True,key=4)
+    Flu = st.checkbox("Flu")
+    if Flu:
+     st.checkbox("yes", value = True,key=5)
+    Fever = st.checkbox("Fever" )
+    if Fever:
+     st.checkbox("yes", value = True,key=6)
+    LowBP = st.checkbox("LowBP" )
+    if LowBP:
+     st.checkbox("yes", value = True,key=7)
+    HighBP= st.checkbox("HighBP")
+    if HighBP:
+     st.checkbox("yes", value = True,key=8)
+    result=""
+    if st.button("Predict"):
+      result=predict_note_authentication(Diabetes_Type1,Diabetes_Type2,liverDisease,heartDisease, kidneyDisease,Flu,Fever,LowBP,HighBP)
+    st.success('Recommended {}'.format(result)) 
+   
+       
 
 if __name__=='__main__':
     main()
