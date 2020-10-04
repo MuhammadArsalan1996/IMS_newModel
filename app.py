3b9b18e5e2ef223c6a392e20c7f2f5c5c9035479
MuhammadArsalan1996 <52852763+MuhammadArsalan1996@users.noreply.github.com>
2020-10-04 05:36:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import numpy as np
 import pickle
 import pandas as pd
 
-
+import streamlit as st 
 
 from PIL import Image
 from flask import Flask, request, jsonify, render_template
@@ -23,6 +23,60 @@ IMS_model = pickle.load(open('ims.pkl', 'rb'))
 @app.route('/')
 def welcome():
     return "Welcome All"
+@app.route('/predict',methods=["Get"])
+def predict_note_authentication(DiabetesTypeOne,DiabetesTypeTwo,liverDisease
+,heartDisease, kidneyDisease,Flu,Fever,LowBP,HighBP):
+
+   
+    prediction=IMS_model.predict([[DiabetesTypeOne,DiabetesTypeTwo,liverDisease
+,heartDisease, kidneyDisease,Flu,Fever,LowBP,HighBP]])
+    print(prediction)
+    return prediction
+
+
+
+def main():
+    st.title("Select your Disease if necessary")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+   
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    DiabetesTypeOne = st.checkbox("DiabetesTypeOne")
+    if DiabetesTypeOne:
+     st.checkbox("yes", value = True,key=0);
+    DiabetesTypeTwo = st.checkbox("DiabetesTypeTwo")
+    if DiabetesTypeTwo:
+     st.checkbox("yes", value = True,key=1)
+    liverDisease = st.checkbox("liverDisease")
+    if liverDisease:
+     st.checkbox("yes", value = True,key=2)
+    heartDisease = st.checkbox("heartDisease")
+    if heartDisease:
+     st.checkbox("yes", value = True,key=3)
+    kidneyDisease = st.checkbox("kidneyDisease")
+    if kidneyDisease:
+     st.checkbox("yes", value = True,key=4)
+    Flu = st.checkbox("Flu")
+    if Flu:
+     st.checkbox("yes", value = True,key=5)
+    Fever = st.checkbox("Fever" )
+    if Fever:
+     st.checkbox("yes", value = True,key=6)
+    LowBP = st.checkbox("LowBP" )
+    if LowBP:
+     st.checkbox("yes", value = True,key=7)
+    HighBP= st.checkbox("HighBP")
+    if HighBP:
+     st.checkbox("yes", value = True,key=8)
+    result=""
+    if st.button("Predict"):
+      result=predict_note_authentication(DiabetesTypeOne,DiabetesTypeTwo,liverDisease,heartDisease, kidneyDisease,Flu,Fever,LowBP,HighBP)
+    st.success('Recommended {}'.format(result)) 
+   
+   
+       
         
         #Mobile App Api
 @app.route('/predict_api',methods=['GET', 'POST'])
